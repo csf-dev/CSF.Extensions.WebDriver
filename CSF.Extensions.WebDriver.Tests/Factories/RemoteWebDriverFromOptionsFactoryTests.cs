f9ed8c8663a3b51b24a49639ef51ed60ee28434b
Craig Fowler <craig@craigfowler.me.uk>
2024-12-07 12:40:45 +0000

    More ignored tests to diagnose #47

--- CSF.Extensions.WebDriver.Tests/Factories/RemoteWebDriverFromOptionsFactoryTests.cs
+++ CSF.Extensions.WebDriver.Tests/Factories/RemoteWebDriverFromOptionsFactoryTests.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium.Remote;
 
 namespace CSF.Extensions.WebDriver.Factories;
 
-[TestFixture,Parallelizable]
+[TestFixture,Parallelizable][Ignore("Temporarily ignored to diagnose #47")]
 public class RemoteWebDriverFromOptionsFactoryTests
 {
     [Test,AutoMoqData]
