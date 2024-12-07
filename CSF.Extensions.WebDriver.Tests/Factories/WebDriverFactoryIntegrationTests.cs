46d9c3c282039646093f25b434de15e699401dcb
Craig Fowler <craig@craigfowler.me.uk>
2024-12-07 12:48:08 +0000

    Re-enabled for #47

--- CSF.Extensions.WebDriver.Tests/Factories/WebDriverFactoryIntegrationTests.cs
+++ CSF.Extensions.WebDriver.Tests/Factories/WebDriverFactoryIntegrationTests.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium;
 
 namespace CSF.Extensions.WebDriver.Factories;
 
-[TestFixture,Parallelizable][Ignore("Temporarily ignored to diagnose #47")]
+[TestFixture,Parallelizable]
 public class WebDriverFactoryIntegrationTests
 {
     [Test]
